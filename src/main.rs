375ce2745baa34b0ba67e84c5e092041b24af761
arewei <arezhiwei@gmail.com>
2021-06-16 17:07:04 +0800

    init upload

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let s1 = String::from("abc");
+
+    {
+        let s2 = &s1;
+        println!("s2 = {}", s2);
+    }
+
+    println!("s1 = {}", s1);
+
+    println!("Hello, world!");
+}
