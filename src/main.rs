d943bd8bffe1ef0d3d131a2afaf32df419203ddd
arewei <arezhiwei@gmail.com>
2021-06-17 16:20:05 +0800

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
+use rand::thread_rng;
+use rand::Rng;
 
 fn main() {
     let rand_string: String = thread_rng()
         .sample_iter(&Alphanumeric)
-        .take(30)
+        .take(32)
         .map(char::from)
         .collect();
 
     println!("{}", rand_string);
-
-
-    
 }
