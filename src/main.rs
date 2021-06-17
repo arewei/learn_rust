7db850e9ff0e80e2b2d9911c8b07507e7f7c0665
arewei <arezhiwei@gmail.com>
2021-06-17 17:27:23 +0800

    move to idea

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-use error_chain::error_chain;
-use rand::distributions::Alphanumeric;
-use rand::thread_rng;
-use rand::Rng;
+extern crate error_chain;
+
 use std::net::IpAddr;
 use std::str;
 
-error_chain! {
-    foreign_links {
-            Utf8(std::str::Utf8Error);
-            AddrParse(std::net::AddrParseError);
-    }
-}
+use error_chain::error_chain;
+use rand::distributions::Alphanumeric;
+use rand::Rng;
+use rand::thread_rng;
 
 fn main() -> Result<()> {
     let rand_string: String = thread_rng()
@@ -23,8 +19,15 @@ fn main() -> Result<()> {
 
     let bytes = b"2001:db8::1";
     let s = str::from_utf8(bytes)?;
-    let addr: IpAddr = s.parse()?;
-    println!("{:?}", addr);
+    let address: IpAddr = s.parse()?;
+    println!("{:?}", address);
 
     Ok(())
+}
+
+error_chain! {
+    foreign_links {
+            Utf8(std::str::Utf8Error);
+            AddrParse(std::net::AddrParseError);
+    }
 }
