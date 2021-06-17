c20064e9fa0298371820dd5ac09989f930744e4c
arewei <arezhiwei@gmail.com>
2021-06-17 17:05:34 +0800

    error chain

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use error_chain::error_chain;
 use rand::distributions::Alphanumeric;
 use rand::thread_rng;
 use rand::Rng;
+use std::net::IpAddr;
+use std::str;
 
-fn main() {
+error_chain! {
+    foreign_links {
+            Utf8(std::str::Utf8Error);
+            AddrParse(std::net::AddrParseError);
+    }
+}
+
+fn main() -> Result<()> {
     let rand_string: String = thread_rng()
         .sample_iter(&Alphanumeric)
         .take(32)
@@ -10,4 +20,11 @@ fn main() {
         .collect();
 
     println!("{}", rand_string);
+
+    let bytes = b"2001:db8::1";
+    let s = str::from_utf8(bytes)?;
+    let addr: IpAddr = s.parse()?;
+    println!("{:?}", addr);
+
+    Ok(())
 }
